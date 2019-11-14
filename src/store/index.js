0450cf0893f52a3a73587eed59b11cf8b3a7046b
Your Name <you@example.com>
2019-11-14 21:06:37 +0800

    操作token的工具函数托管给vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,36 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+// 把操作token的工具函数托管给vuex 因为组件要共享token(许多组件都需要用)
+// - 共享token数据
+// - 在vuex中更好操作，更好监听。
+// 指定成员导入
+// import {getUser}from '@/utils/auth'
+// 全部成员导入
+import * as auth from '@/utils/auth'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+
   state: {
+    // 用户信息
+    user: auth.getUser()
   },
   mutations: {
+    // 修改用户信息(设置用户信息)
+    // 默认传参+新的用户信息 user是对象(信息很多)
+    setUser (state, user) {
+      // 只修改了vuex里的数据
+      // 如果刷新页面,代码重新执行,会再从本地拿去信息,
+      state.user = user
+      // 所以 在修改用户信息时要 同时 更新本地用户信息
+      auth.setUser(user)
+    },
+    // 删除用户信息
+    delUser (state) {
+      state.delUser = {}
+      // 更新本地用户信息
+      auth.delUser()
+    }
   },
-  actions: {
-  }
+  actions: {}
 })
--- src/utile/auth.js
+++ src/utile/auth.js
@@ -1,9 +1,9 @@
-// auth 权限认证的意思  token 令牌
+// auth 权限认证的意思  token 令牌  封装工具模块
 // 提供  获取 存储 删除 token   拿到的使用户信息,但主要使用token
 // 设置key
 const USER_KEY = 'hm-toutiao-m-84-user'
 
-// 获取token
+// 其实是拿用户信息 包括token  获取token
 // 指定成员导出
 export const getUser = () => {
   // 使用localStorage可以关闭浏览器后不清除token  拿出的的是字符串 需要转成对象
