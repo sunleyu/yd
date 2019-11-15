e9bb5c8f7f0510e48990f3f5e9fb8494cfa58848
Your Name <you@example.com>
2019-11-15 16:13:42 +0800

    登录界面基本布局及登录拦截

--- src/views/user/login.vue
+++ src/views/user/login.vue
@@ -1,7 +1,20 @@
 <template>
-  <div class="container">
-     <van-nav-bar left-arrow @click-left="$router.back()" title="登录"></van-nav-bar>
-  </div>
+
+ <div class="page-login">
+    <van-nav-bar left-arrow @click-left="$router.back()" title="登 录"></van-nav-bar>
+    <van-cell-group>
+        <van-field  label="手机号" placeholder="请输入手机号" />
+        <van-field label="验证码" placeholder="请输入验证码">
+            <van-button class="p5" slot="button" size="mini" type="primary">
+                发送验证码
+            </van-button>
+        </van-field>
+    </van-cell-group>
+    <div class="btn_box">
+        <van-button type="info" @click="login" block round>登 录</van-button>
+    </div>
+</div>
+
 </template>
 
 <script>
---
+++ src/vue.config.js
@@ -0,0 +1,13 @@
+// https://cli.vuejs.org/zh/config/#css-loaderoptions
+module.exports = {
+  // 覆盖蓝色主题
+  css: {
+    loaderOptions: {
+      less: {
+        modifyVars: {
+          blue: '#3296fa'
+        }
+      }
+    }
+  }
+}
