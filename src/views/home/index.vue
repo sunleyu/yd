c72d489722dea7a7c6abeab68b97b4a1565eb8a2
Your Name <you@example.com>
2019-11-16 15:11:19 +0800

    主页基本布局完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="container">
-    home
+    <!-- swipeable 是否开启手势滑动切换 -->
+  <van-tabs swipeable>
+  <van-tab :key="index" v-for="index in 8" :title="'标签 ' + index">
+    <div class="scroll-wrapper">
+     <van-cell-group>
+         <van-cell v-for="item in 20" :key="item">{{index}}</van-cell>
+      </van-cell-group>
+     </div>
+  </van-tab>
+</van-tabs>
+<span class="bar_btn" slot="nav-right">
+  <van-icon name="wap-nav"></van-icon>
+</span>
   </div>
 </template>
 
@@ -11,4 +23,64 @@ export default {
 </script>
 
 <style scoped lang="less">
+// - /deep/  作用：让样式在其他组件生效。
+// - 保证 scroll-wrapper 容器能够生成滚动条，将来需要使用滚动。
+//   - 下拉刷新
+//   - 上拉加载
+  // - 记录浏览位置
+.van-tabs {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  /deep/ .van-tabs__wrap {
+    height: 36px;
+    padding-right: 36px;
+    .van-tab {
+      line-height: 36px;
+    }
+    .van-tabs__line {
+      background-color: #3296fa;
+      height: 2px;
+    }
+  }
+  /deep/ .van-tabs__content{
+    flex: 1;
+    overflow: hidden;
+  }
+  /deep/ .van-tab__pane{
+    height: 100%;
+    .scroll-wrapper{
+      height: 100%;
+      overflow-y: auto;
+    }
+  }
+}
+.bar_btn {
+  width: 36px;
+  height: 35px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  &::before {
+    content: "";
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: 999;
+    box-shadow: 0 0 10px #999;
+    transform: scale(1, 0.6);
+  }
+  .van-icon-wap-nav {
+    width: 100%;
+    height: 100%;
+    background: #fff;
+    text-align: center;
+    line-height: 35px;
+    position: relative;
+    z-index: 1000;
+    &::before {
+      font-size: 20px;
+    }
+  }
+}
 </style>
